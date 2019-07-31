2af472649eed59ad84d66f2bd6e3d9f38c46667d
Justin Friebel <friebj99@gmail.com>
2019-07-31 01:58:09 -0500

    Add menu links.

--- src/components/header.js
+++ src/components/header.js
@@ -1,38 +1,65 @@
 import React from "react"
 import Link from "gatsby-link"
+import styled from "styled-components"
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: "#0C5900",
-    }}
-  >
-    <div
-      style={{
-        margin: "0 auto",
-        maxWidth: 960,
-        padding: "1.45rem 1.0875rem",
-      }}
-    >
-      <div style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: "#B8B233",
-            textDecoration: "none",
-            fontFamily:
-              "-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif",
-            fontWeight: "bold",
-            textRendering: "optimizeLegibility",
-            fontSize: "1.25rem",
-            lineHeight: "1.1",
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </div>
-    </div>
-  </div>
+  <StyledHeader>
+    <StyledLogo to="/">{siteTitle}</StyledLogo>
+    <StyledNav>
+      <StyledMenuItemsWrapper>
+        <StyledMenuItem to="/about">About</StyledMenuItem>
+        <StyledMenuItem to="/services">Services</StyledMenuItem>
+        <StyledMenuItem to="/projects">Projects</StyledMenuItem>
+        <StyledMenuItem to="/contact">Contact</StyledMenuItem>
+      </StyledMenuItemsWrapper>
+    </StyledNav>
+  </StyledHeader>
 )
+
+const StyledHeader = styled.header`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  padding: 20px 10px;
+  border-bottom: 1px solid #ccc;
+  background-color: #0c5900;
+`
+
+const StyledLogo = styled(Link)`
+  color: #b8b233;
+  text-decoration: none;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  font-weight: bold;
+  text-rendering: optimizeLegibility;
+  font-size: 1.25rem;
+  line-height: 1.1;
+`
+
+const StyledNav = styled.nav`
+  margin-left: auto;
+  line-height: 1.1;
+`
+
+const StyledMenuItemsWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  padding: 0;
+  margin: 0;
+`
+
+const StyledMenuItem = styled(Link)`
+  margin-left: 16px;
+  list-style: none;
+  margin-bottom: 0px;
+  color: #fff;
+  text-decoration: none;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  font-weight: normal;
+  text-rendering: optimizeLegibility;
+  font-size: 1rem;
+  line-height: 1.1;
+`
 
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,10 @@ const IndexPage = () => (
     <SEO title="Home" />
     <div className="flex-row">
       <div className="flex-column">
-        <h1>Building & Remodeling Homes around Oshkosh since 1976</h1>
+        <h1>
+          Building & remodeling homes & businesses in Fox Valley Wisconsin since
+          1976
+        </h1>
       </div>
       <div className="flex-column">
         <ChrisDave />
@@ -18,6 +21,33 @@ const IndexPage = () => (
     </div>
     <div className="measuring-tape">
       <MeasuringTape />
+    </div>
+    <div
+      style={{
+        margin: "0 auto",
+        maxWidth: 960,
+        padding: "1.45rem 1.0875rem",
+      }}
+    >
+      <p>
+        Packer Valley Builders is a fully licensed and insured General
+        Contractor offering serves to both Residential and Commercial clients in
+        the Fox Valley area. While every project is different, we know that the
+        most important one is yours. We oversee all phases of construction
+        projects as your General Contractor.
+      </p>
+      <p>
+        Starting from your ideas to the completion. We can help you increase the
+        value of your property by adding just the right touches that are
+        customized to your specifications. Packer Valley Builders, Inc. is an
+        experienced team that will complete your project on time and with
+        quality results. In order to succeed in the construction industry, a
+        general contractor must integrate superior craftsmanship, experience,
+        and dedication within the company. We believe that teamwork, integrity,
+        honesty and attention to detail each step of the way, make the best
+        construction environment for everyone; which ultimately leads to
+        customer satisfaction.
+      </p>
     </div>
     {/* <div
       className="flex-row"
